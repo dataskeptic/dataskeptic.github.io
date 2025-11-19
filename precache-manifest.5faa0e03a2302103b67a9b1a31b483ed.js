710e5d1aac70a23486b80fb87644b729552c2047
Mateus Henrique <mathenrique@ufpi.edu.br>
2025-11-19 19:12:53 -0300

    Updates

---
+++ precache-manifest.5faa0e03a2302103b67a9b1a31b483ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d550a8595212083008be3addfb5e26e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57c0bc8e7c7893c2e9ca",
+    "url": "/static/css/2.7a519478.chunk.css"
+  },
+  {
+    "revision": "5b011278bcba22d3ac50",
+    "url": "/static/css/main.4926842a.chunk.css"
+  },
+  {
+    "revision": "57c0bc8e7c7893c2e9ca",
+    "url": "/static/js/2.fc3338de.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.fc3338de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b011278bcba22d3ac50",
+    "url": "/static/js/main.fbb47936.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b891116c2e4c3867db8cd0c3a8077ae9.js"
+  "/precache-manifest.5faa0e03a2302103b67a9b1a31b483ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
